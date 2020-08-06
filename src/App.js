c50d3809f03fc9595e71224a4047759e03b4c357
genisalesjr <genisalesjr@gmail.com>
2020-08-06 12:31:28 -0300

    Setup inicial do projeto

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Menu from './components/Menu'
+
+
+function App() {
+  return (
+    <div>
+      <Menu/>
+
+      
+    
+    </div>
+  );
+}
+
+export default App;
